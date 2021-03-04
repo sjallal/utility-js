c790d194c79e5d5dacf4953047297d8402ba0cbe
Aqib Jallal <aqibjallal@Aqibs-MacBook-Air.local>
2021-03-04 19:46:23 +0530

    Added tests for [a{x : 10}],someObject => someObject.x + 1]

--- src/map.spec.js
+++ src/map.spec.js
@@ -14,4 +14,8 @@ describe("Map", () => {
   it("map([1,2,3],identity is [1,2,3]", () => {
     expect(map([1, 2, 3], identity)).toEqual([1, 2, 3]);
   });
+
+  it("map of [a{x : 10}],someObject => someObject.x + 1] is [11]", () => {
+    expect(map([{ x: 10 }], (someObject) => someObject.x + 1)).toEqual([11]);
+  });
 });
