ec710f19cb86dd03e107959a474a23555f548364
Aqib Jallal <aqibjallal@Aqibs-MacBook-Air.local>
2021-03-04 19:43:31 +0530

    Added tests for map-identity function

--- src/map.spec.js
+++ src/map.spec.js
@@ -1,5 +1,6 @@
 const map = require("./map");
 const cube = require("./cube");
+const identity = require("./identity");
 
 describe("Map", () => {
   it("map([], cube) is []", () => {
@@ -8,5 +9,9 @@ describe("Map", () => {
 
   it("map([1,2,3],cube is [1,8,27]", () => {
     expect(map([1, 2, 3], cube)).toEqual([1, 8, 27]);
+  });
+
+  it("map([1,2,3],identity is [1,2,3]", () => {
+    expect(map([1, 2, 3], identity)).toEqual([1, 2, 3]);
   });
 });
