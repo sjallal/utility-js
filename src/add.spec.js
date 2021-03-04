90c7133536b51a12218069a0e295935008805cce
sjallal <sjallal88@gmail.com>
2021-03-04 12:22:08 +0530

    Fixed the expected value to 1

--- src/add.spec.js
+++ src/add.spec.js
@@ -1,13 +1,11 @@
-const add = require('./add');
+const add = require("./add");
 
-describe('Add', () => {
+describe("Add", () => {
+  it("Sum of 0 and 0 is 0", () => {
+    expect(add(0, 0)).toEqual(0);
+  });
 
-    it('Sum of 0 and 0 is 0', () => {
-        expect(add(0, 0)).toEqual(0);
-    });
-
-    it('Sum of 1 and 0 is 1', () => {
-        expect(add(1, 0)).toEqual(0);
-    });
-
-})
+  it("Sum of 1 and 0 is 1", () => {
+    expect(add(1, 0)).toEqual(1);
+  });
+});
