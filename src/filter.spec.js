576ddf9223a8e4e568406af5866aeb2d02d99c51
Saqib Jallal <sjallal88@gmail.com>
2021-03-05 09:05:58 +0530

    Added all the tests for filter function

--- src/filter.spec.js
+++ src/filter.spec.js
@@ -1,7 +1,24 @@
 const filter = require("./filter");
+const filterUpperCase = require("./filterUpperCase");
 
 describe("Filter", () => {
   it("filter([], x=>true) is []", () => {
     expect(filter([], (x) => true)).toEqual([]);
+  });
+
+  it("filter([1,2,3], x=>true) is [1,2,3]", () => {
+    expect(filter([1, 2, 3], (x) => true)).toEqual([1, 2, 3]);
+  });
+
+  it("filter([1,2,3], x=>false) is []", () => {
+    expect(filter([1, 2, 3], (x) => false)).toEqual([]);
+  });
+
+  it("filter([1,2,3], x => x > 1) is [2, 3]", () => {
+    expect(filter([1, 2, 3], (x) => x > 1)).toEqual([2, 3]);
+  });
+
+  it("filter(['a', 'B', 'c', 'D'], filterUpperCase) is ['B', 'D']", () => {
+    expect(filter(["a", "B", "c", "D"], filterUpperCase)).toEqual(["B", "D"]);
   });
 });
